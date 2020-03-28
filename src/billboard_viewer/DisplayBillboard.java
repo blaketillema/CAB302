d11d047eb591b59a442566616e855300d38dfef8
Dan <daniel.nehl.it@gmail.com>
2020-03-28 21:28:56 +1000

    Initial Classes

---
+++ src/billboard_viewer/DisplayBillboard.java
@@ -0,0 +1,29 @@
+package billboard_viewer;
+
+import ...
+
+public class DisplayBillboard {
+    // GUI
+    public void displayCurrentBillboard() {
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        // get billboard data
+
+
+        /** Call to refresh the screen display to enact update
+         */
+        displayCurrentBillboard();
+
+    }
+
+}
+
+
+
+
+
