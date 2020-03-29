bd882ee05ce7f00f230dad8aea0003bdc2c4447a
VernonJohnson <thomas.vernon.johnson@gmail.com>
2020-03-29 17:46:59 +1000

    Add ServerConnect feature

--- src/Sandbox/sandboxMain.java
+++ src/Sandbox/sandboxMain.java
@@ -1,5 +1,10 @@
 package Sandbox;
 
+import billboard_viewer.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class sandboxMain {
 
     public static void main(String[] args) {
@@ -7,10 +12,44 @@ public class sandboxMain {
 
         System.out.println(System.getProperty("user.dir")); // Output 'working directory' e.g. C:\java\project
 
-        new gui(); // GUI class for testing billboard features
-        new readXML(); // Class for figuring out parsing XML
+        //new gui(); // GUI class for testing billboard features
+        //new readXML(); // Class for figuring out parsing XML
+        //billboard_viewer.serverConnect serverBillboard = new billboard_viewer.serverConnect();
+        TreeMap<String, String> treeMap = new TreeMap<String, String>();
+        TreeMap<String, String> billboardMap = new TreeMap<String, String>();
 
+        /**
+         * Start the Server Connect as a new thread
+         */
+        ServerConnect connect;
+        Thread connectThread = new Thread(connect = new ServerConnect()); // Create a thread
+        connectThread.start(); // Start the thread, will run every 15 seconds
 
-        System.out.println("----- SANDBOX END -----");
+        /**
+         * Pull billboard TreeMap information from running thread
+         */
+        billboardMap = connect.getBillboard(); // get the current TreeMap
+        connect.printBillboard(); // Print the current TreeMap to terminal
+
+        /**
+         * Get billboard every 15 seconds
+         */
+        while(true) {
+            //
+            treeMap = connect.getBillboard();
+            //System.out.println("The value for the background key is " + treeMap.get("billboardBackground"));
+            //System.out.println("The value for the message key is " + treeMap.get("message"));
+            connect.printBillboard();
+            try {
+                Thread.sleep(15000);
+                //
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //System.out.println("----- SANDBOX END -----");
     }
+
+
 }
