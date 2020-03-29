15dd54010346026913047aca92f3a58bfa062319
Dan <daniel.nehl.it@gmail.com>
2020-03-29 17:03:51 +1000

    Change billboard constructor (TreeMap<String, String> content) & classes
    1) Billboard constructor now  takes a  (TreeMap<String, String> content) 
    2)  Changed class structure to seperate Main/Run Class
    3)  Now can use overloading to display different types of billboard

--- src/billboard_viewer/Billboard.java
+++ src/billboard_viewer/Billboard.java
@@ -1,5 +1,25 @@
 package billboard_viewer;
 
-public class Billboard {
+
+import java.util.TreeMap;
+
+public abstract class Billboard {
+
+
+
+    /*
+    public TreeMap<String, String> getBillboardContents(CurrentBillboard billboard) {
+        TreeMap<String, String> billboardContents = new TreeMap<>();
+        if (billboard.getBillboardMessage() != null) {
+            billboardContents.put("message", billboard.getBillboardMessage());
+        }
+        if (billboard.getBillboardMessage() != null) {
+            billboardContents.put("base64Image", billboard.getBillboardBase64Image());
+        }
+        return billboardContents;
+    }
+    */
+
+
 
 }
