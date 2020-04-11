9941adc2cae000dd74999e713c98f77034a6cdfa
VernonJohnson <thomas.vernon.johnson@gmail.com>
2020-04-11 14:35:33 +1000

    more billboard panel types implemented, added some TODO notes

--- src/billboard_viewer/MainRunDisplay.java
+++ src/billboard_viewer/MainRunDisplay.java
@@ -4,7 +4,9 @@ import java.text.MessageFormat;
 import java.util.Map;
 import java.util.TreeMap;
 
- /**
+import static java.lang.Thread.sleep;
+
+/**
  * This is the main class to run the program
  */
 public class MainRunDisplay {
