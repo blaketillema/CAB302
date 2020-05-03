5446b19b808628fbc77d120bf679c82efbcc0846
VernonJohnson <thomas.vernon.johnson@gmail.com>
2020-05-04 01:10:07 +1000

    XML import and export feature completion and billboard builder UI creation

--- src/billboard_control_panel/BillboardControl.java
+++ src/billboard_control_panel/BillboardControl.java
@@ -1,36 +1,103 @@
 package billboard_control_panel;
 
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Set;
 import java.util.TreeMap;
 
+import billboard_viewer.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
+
+import org.w3c.dom.*;
+
+// TODO - cleanup and documentation of BillboardControl class
+// TODO - cleanup of unused UI elements and appropriate element wrapping/visual presentation
+
 public class BillboardControl {
-    private JTextField textField1;
+    private JTextField billboardNameArea;
     private JButton importXMLButton;
     private JButton exportXMLButton;
+    private JButton unused1;
+    private JButton unused2;
     private JButton uploadImageButton;
-    private JButton textFontButton;
-    private JButton textSizeButton;
-    private JButton textColourButton;
-    private JButton backgroundColourButton;
+    private JButton applyButton;
     private JButton exitButton;
     private JButton saveButton;
     private JButton previewButton;
     private JPanel billboardControl;
     private JEditorPane editorPane1;
+    private JTextArea messageArea;
+    private JTextArea messageColourArea;
+    private JTextArea informationArea;
+    private JTextArea informationColourArea;
+    private JTextArea backgroundColourArea;
+    private JTextArea pictureUrlArea;
+    private JTextArea pictureDataArea;
+
+    TreeMap<String, String> currentBillboard = new TreeMap<>(); // Initialize an empty TreeMap
 
     public BillboardControl() {
         importXMLButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String paneText = null;
+                paneText = editorPane1.getText();
+                System.out.println("paneText: " + paneText);
+
+                //TreeMap billboardMap = new TreeMap<String, String>();
+
+                // Convert string to document
+                Document doc = null; // Initialize document
+
+                DocumentBuilder documentBuilder = null;
+                try {
+                    documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                    doc = documentBuilder.parse(new ByteArrayInputStream(paneText.getBytes("UTF-8")));
+                } catch (ParserConfigurationException | UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                } catch (SAXException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                //billboardMap = parseBillboard(doc);
+                currentBillboard = parseBillboard(doc);
+
+                // Refresh text fields
+                refreshFields();
+
+                printBillboard(currentBillboard);
+
             }
         });
         exportXMLButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                // Call XML creation method
+                editorPane1.setText(createXML());
 
             }
         });
@@ -39,33 +106,7 @@ public class BillboardControl {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Preview Billboard");
 
-                //TODO - properly implement preview, currently concept test code
-
-                System.out.println("Import XML");
-
-                // TEST CODE
-                TreeMap<String, String> sampleBillboard = new TreeMap<>();
-
-                String billboardBackground = "#0000FF";
-                String message = "THIS IS A PREVIEW BUTTON TEST";
-                String messageColour = "#FFFF00";
-                String pictureUrl = "https://www.w3schools.com/html/pic_trulli.jpg";
-                String pictureData = null;
-                String information = "Be sure to check out https://example.com/ for more information." +
-                        "Longer Information string for testing: Hello world. Hello World. Hello World." +
-                        "Hello World. Hello World. Hello World. INFORMATION END";
-                String informationColour = "#00FFFF";
-
-                sampleBillboard.put("billboardBackground", billboardBackground);
-                sampleBillboard.put("message", message);
-                sampleBillboard.put("messageColour", messageColour);
-                sampleBillboard.put("pictureUrl", pictureUrl);
-                sampleBillboard.put("pictureData", pictureData);
-                sampleBillboard.put("information", information);
-                sampleBillboard.put("informationColour", informationColour);
-
-                // Pass in TreeMap contents of a billboard to the Billboard class
-                billboard_viewer.Billboard previewBillboard = new billboard_viewer.Billboard(sampleBillboard, true);
+                Billboard previewBillboard = new Billboard(currentBillboard, true);
 
             }
         });
@@ -90,36 +131,41 @@ public class BillboardControl {
                 }
             }
         });
-        textFontButton.addActionListener(new ActionListener() {
+        unused1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-        textSizeButton.addActionListener(new ActionListener() {
+        unused2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
-        textColourButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
 
-            }
-        });
-        backgroundColourButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        // TODO - Implement image upload and conversion to BASE64
+        /**
+         * Convert provided image file (png,jpg) to BASE64 Format
+         */
         uploadImageButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+
+
             }
         });
+        applyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Apply Changes
+                applyChanges();
+            }
+        });
+
+
+
     }
     public static void main(String[] args) {
         /* Create and display the form */
@@ -130,4 +176,266 @@ public class BillboardControl {
         frame.pack();
         frame.setVisible(true);
     }
+
+    private String createXML() {
+        // parse billboard treemap to XML
+
+        applyChanges(); // Apply changes from fields, will set empty fields to null
+
+        String xmlExport = null;
+
+
+        try {
+            // Setup builders
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            // Create root element for billboard xml
+            Element billboardRoot = document.createElement("billboard");
+            document.appendChild(billboardRoot);
+
+            // Handle background colour attribute
+            if (currentBillboard.get("billboardBackground") != null) {
+                // Add attribute background colour to root element
+                Attr backgroundAttr = document.createAttribute("background");
+                backgroundAttr.setValue(currentBillboard.get("billboardBackground"));
+                billboardRoot.setAttributeNode(backgroundAttr);
+            }
+
+            // Handle message and messageColour attribute
+            if (currentBillboard.get("message") != null) { // Check if message exists
+                // Add message element
+                Element message = document.createElement("message");
+                message.appendChild(document.createTextNode(currentBillboard.get("message")));
+
+                if (currentBillboard.get("messageColour") != null) { // Check if message colour exists
+                    // Add colour attribute
+                    Attr messageColour = document.createAttribute("colour");
+                    messageColour.setValue(currentBillboard.get("messageColour"));
+                    message.setAttributeNode(messageColour);
+                }
+                billboardRoot.appendChild(message); // add message
+            }
+
+            // Handle Picture (URL or Data)
+            if (currentBillboard.get("pictureUrl") != null ) { // Add picture url attribute
+                Element picture = document.createElement("picture");
+                Attr pictureUrl = document.createAttribute("url");
+                pictureUrl.setValue(currentBillboard.get("pictureUrl"));
+                picture.setAttributeNode(pictureUrl);
+                billboardRoot.appendChild(picture);
+            } else {
+                if (currentBillboard.get("pictureData") != null) { // Add picture data attribute
+                    Element picture = document.createElement("picture");
+                    Attr pictureUrl = document.createAttribute("data");
+                    pictureUrl.setValue(currentBillboard.get("pictureData"));
+                    picture.setAttributeNode(pictureUrl);
+                    billboardRoot.appendChild(picture);
+                }
+            }
+
+            // Handle information and information colour attribute
+            if (currentBillboard.get("information") != null) { // Check if message exists
+                // Add message element
+                Element information = document.createElement("information");
+                information.appendChild(document.createTextNode(currentBillboard.get("information")));
+
+                if (currentBillboard.get("informationColour") != null) { // Check if message colour exists
+                    // Add colour attribute
+                    Attr informationColour = document.createAttribute("colour");
+                    informationColour.setValue(currentBillboard.get("informationColour"));
+                    information.setAttributeNode(informationColour);
+                }
+                billboardRoot.appendChild(information); // add message
+            }
+            // End XML elements
+
+            // Transform XML document into a string
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer;
+            transformer = transformerFactory.newTransformer();
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(writer));
+            xmlExport = writer.getBuffer().toString(); // assign to string
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+        if (xmlExport == null) {
+            xmlExport = "Error: Invalid data";
+        }
+
+        return xmlExport;
+    }
+
+    private TreeMap parseBillboard(Document doc) {
+        // implementation attempt
+        // Initialize possible variables;
+        String billboardBackground = null;
+        String message = null;
+        String messageColour = null;
+        String pictureUrl = null; // Picture can be a URL or Data
+        String pictureData = null;
+        String information = null;
+        String informationColour = null;
+
+        NodeList nodeList = doc.getElementsByTagName("billboard");
+
+        for (int i = 0; i <= nodeList.getLength() - 1; i++) {
+            // Iterate nodes in NodeList (should only be one)
+            Node node = nodeList.item(i);
+
+            // Start with billboard background attribute
+            NamedNodeMap attributes = node.getAttributes();
+
+            // Check if billboard background is set, else leave null
+            if (node.getAttributes().item(0).getNodeName() == "background") {
+                // Background attribute name found
+                billboardBackground = node.getAttributes().item(0).getNodeValue();
+            } else {
+                billboardBackground = null;
+            }
+
+            // Iterate child nodes
+            NodeList childList = node.getChildNodes();
+            //System.out.println("LENGTH: " + childList.getLength());
+            for (int k = 0; k <= childList.getLength() - 1; k++) {
+                // iterate through child nodes and find values
+                Node childNode = childList.item(k);
+
+                // Check node
+                //System.out.println("getNodeName: "+ childNode.getNodeName());
+                //System.out.println("getNodeValue: "+ childNode.getNodeValue());
+
+                if (childNode.getNodeName() == "message") {
+                    //
+                    message = childNode.getTextContent();
+                    // Check for message colour attribute
+                    if (childNode.getAttributes().item(0).getNodeName() == "colour") {
+                        // Assign colour
+                        messageColour = childNode.getAttributes().item(0).getNodeValue();
+                    }
+                } else {
+                    if (childNode.getNodeName() == "picture") {
+                        //
+                        //System.out.println("PICTURE");
+                        //System.out.println("PICTURE value: "+ childNode.getNodeValue());
+
+                        if (childNode.getAttributes().item(0).getNodeName() == "url") {
+                            // Assign picture URL
+                            pictureUrl = childNode.getAttributes().item(0).getNodeValue();
+                        } else {
+                            if (childNode.getAttributes().item(0).getNodeName() == "data") {
+                                // assign picture Data
+                                pictureData = childNode.getAttributes().item(0).getNodeValue();
+                            }
+                        }
+                    } else {
+                        if (childNode.getNodeName() == "information") {
+                            //
+                            information = childNode.getTextContent();
+
+                            if (childNode.getAttributes().item(0).getNodeName() == "colour") {
+                                // Assign colour
+                                informationColour = childNode.getAttributes().item(0).getNodeValue();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Parse XML into HashMap
+        TreeMap xmlTreeMap = new TreeMap<String, String>();
+
+        xmlTreeMap.put("billboardBackground", billboardBackground);
+        xmlTreeMap.put("message", message);
+        xmlTreeMap.put("messageColour", messageColour);
+        xmlTreeMap.put("pictureUrl", pictureUrl);
+        xmlTreeMap.put("pictureData", pictureData);
+        xmlTreeMap.put("information", information);
+        xmlTreeMap.put("informationColour", informationColour);
+
+        return xmlTreeMap;
+    }
+
+    /**
+     * Refreshes text area fields when changes are made or applied to the billboard data
+     */
+    private void refreshFields() {
+        // Refresh all text fields
+        messageArea.setText(currentBillboard.get("message"));
+        messageColourArea.setText(currentBillboard.get("messageColour"));
+        informationArea.setText(currentBillboard.get("information"));
+        informationColourArea.setText(currentBillboard.get("informationColour"));
+        backgroundColourArea.setText(currentBillboard.get("billboardBackground"));
+        pictureUrlArea.setText(currentBillboard.get("pictureUrl"));
+        pictureDataArea.setText(currentBillboard.get("pictureData"));
+    }
+
+    /**
+     * Applies the changes from JTextFields to the currentBillboard treemap,
+     * Applies variables based on text field contents, assigning null if empty.
+     */
+    private void applyChanges() {
+        // Initialize billboard variables as null
+        String message = null;
+        String messageColour = null;
+        String information = null;
+        String informationColour = null;
+        String billboardBackground = null;
+        String pictureUrl = null; // Picture can be a URL or Data
+        String pictureData = null;
+
+        // Check if text boxes have content and assign variables, else leave variable as null
+        if (messageArea.getText().length() > 0) { // Check not empty
+            message = messageArea.getText();
+        } // else leave as null
+        if (messageColourArea.getText().length() > 0) { // Check not empty
+            messageColour = messageColourArea.getText();
+        } // else leave as null
+        if (informationArea.getText().length() > 0) { // Check not empty
+            information = informationArea.getText();
+        } // else leave as null
+        if (informationColourArea.getText().length() > 0) { // Check not empty
+            informationColour = informationColourArea.getText();
+        } // else leave as null
+        if (backgroundColourArea.getText().length() > 0) { // Check not empty
+            billboardBackground = backgroundColourArea.getText();
+        } // else leave as null
+        if (pictureUrlArea.getText().length() > 0) { // Check not empty
+            pictureUrl = pictureUrlArea.getText();
+        } // else leave as null
+        if (pictureDataArea.getText().length() > 0) { // Check not empty
+            pictureData = pictureDataArea.getText();
+        } // else leave as null
+
+        // Apply changes into to the TreeMap object
+        currentBillboard.put("billboardBackground", billboardBackground);
+        currentBillboard.put("message", message);
+        currentBillboard.put("messageColour", messageColour);
+        currentBillboard.put("pictureUrl", pictureUrl);
+        currentBillboard.put("pictureData", pictureData);
+        currentBillboard.put("information", information);
+        currentBillboard.put("informationColour", informationColour);
+    }
+
+    /**
+     * TEST CLASS FOR PRINTING TREEMAP / BILLBOARD CONTENTS
+     * @param billboardMap
+     */
+    public void printBillboard(TreeMap<String, String> billboardMap) {
+        System.out.println("Connect: Printing Billboard...");
+        Set<String> set1 = billboardMap.keySet();
+        for (String key: set1) {
+            System.out.println("Connect Key : "  + key + "\t\t" + "Value : "  + billboardMap.get(key));
+        }
+    }
+
 }
