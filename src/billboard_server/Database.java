8700c82efa66618191d975f5d314b85a46bfdbfc
Blake <blaketillema@gmail.com>
2020-05-31 17:35:35 +1000

    another fix

--- src/billboard_server/Database.java
+++ src/billboard_server/Database.java
@@ -93,27 +93,25 @@ public class Database {
      * @throws SQLException
      */
     private void setup() throws SQLException { //Runs a setup SQL statement, creating the users table. This is run during construction
-        dropDb();
-
-        // Default admin account
         String adminUserID = "b220a053-91f1-48ee-acea-d1a145376e57";
         String adminSalt = "2219d4ec595ce93cabfe7c7941d7e274";
         String adminHash = UserAuth.hashAndSalt("7ec582cb6dda5f00485d4f3026c1309ba7c3eb255cdfbdcb4a3fb3646d74953d", adminSalt);
-
         connect();
         statement.executeQuery(USERS_TABLE);
         statement.executeQuery(BILLBOARDS_TABLE);
         statement.executeQuery(SCHEDULE_TABLE);
-
-        pstmt = conn.prepareStatement(adduserStatement);
-        pstmt.setString(1, adminUserID);
-        pstmt.setString(2, "admin");
-        pstmt.setString(3, adminHash);
-        pstmt.setString(4, adminSalt);
-        pstmt.setInt(5, Protocol.Permission.ALL);
-        pstmt.execute();
-
         conn.close();
+        if(!doesUserExist(adminUserID)) {
+            connect();
+            pstmt = conn.prepareStatement(adduserStatement);
+            pstmt.setString(1, adminUserID);
+            pstmt.setString(2, "admin");
+            pstmt.setString(3, adminHash);
+            pstmt.setString(4, adminSalt);
+            pstmt.setInt(5, Protocol.Permission.ALL);
+            pstmt.execute();
+            conn.close();
+        }
     }
 
     /**
